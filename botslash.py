d637b038c126defb2f3ea4e44c271d69a90f38b1
Gia <griffindugan49@gmail.com>
2022-08-20 23:43:13 -0400

    all commands ported over

--- botslash.py
+++ botslash.py
@@ -107,7 +107,7 @@ print(stored)
 # stored[key]["allies"]
 # stored layout ==      stored = {"name" : {"owner":"x", "allies":["x","y"], ...}, "name2" : and so on}
 
-nations = {key: Nation(key, stored[key]["owner"], []) for key in stored} # creates nation class for each nation
+nations = {key: Nation(key, stored[key]["owner"], stored[key]["allies"]) for key in stored} # creates nation class for each nation
 
 
 
@@ -133,39 +133,119 @@ gnq1 = 'What is the nation called?'
 
 
 # creating a nation in class nation
-@tree.command(name = "createnation", description="Creates a nation in the database", guild = discord.Object(id=1010602651060277279))
+@tree.command(name = "createnation", description="Creates a nation in the database.", guild = discord.Object(id=1010602651060277279))
 async def self(interaction: discord.Interaction, name: str, owner: str):
-        nations[name] = Nation(name, owner) # nation created  
+    nations[name] = Nation(name, owner) # nation created  
 
-        stored[nations[name].name] = {"owner" : nations[name].owner} # nation added to storage dictionary
-        # storing all the nations and info
-        with open(nationFile, "wb") as tf:
-            pickle.dump(stored,tf)
-            tf.close()
-        await interaction.response.send_message("The nation " + name + " has been added to the database.")
+    stored[nations[name].name] = {"owner" : nations[name].owner} # nation added to storage dictionary
+    # storing all the nations and info
+    with open(nationFile, "wb") as tf:
+        pickle.dump(stored,tf)
+        tf.close()
+    await interaction.response.send_message("The nation " + name + " has been added to the database.")
 
 # shows all the commands accessible
-@tree.command(name = "help", description="Lists all the commands possible with Conwylia Bot", guild = discord.Object(id=1010602651060277279))
+@tree.command(name = "help", description="Lists all the commands possible with Conwylia Bot.", guild = discord.Object(id=1010602651060277279))
 async def self(interaction: discord.Interaction):
     await interaction.response.send_message("""
-                            The following commands are active: 
-                            /createNation (Name Of Nation) (Owner Of Nation) -- Creates a nation in the database. 
-                            /help -- Lists all the commands possible with this bot.
-                            /nation (Name of Nation) -- Looks up info on a particular nation in the database. 
-                            &nations -- Lists all the nations in the database. 
-                            &deleteNation -- Deletes a nation from the database.
-                            &ally -- Allies with another nation in the database.
-                            &allies -- Lists all the allies of a nation in the database.
-                            &removeAlly -- Removes an allies between two nations.
-                            """)
+The following commands are active: 
+```
+/createNation (Name Of Nation) (Owner Of Nation)     -- Creates a nation in the database. 
+/help                                                -- Lists all the commands possible with this bot.
+/nation (Name of Nation)                             -- Looks up info on a particular nation in the database. 
+/nations                                             -- Lists all the nations in the database. 
+/deleteNation (Name of Nation)                       -- Deletes a nation from the database.
+/ally (Name of Nation) (Name of Allied Nation)       -- Allies with another nation in the database.
+/allies (Name of Nation)                             -- Lists all the allies of a nation in the database.
+/removeAlly (Name of Nation) (Name of Allied Nation) -- Removes an allies between two nations.
+```
+""")
  
 # all the information about a nation
-@tree.commmand(name = "nation", description = "Shows information about a nation", guild = discord.Object(id=1010602651060277279))
+@tree.command(name = "nation", description = "Shows information about a nation.", guild = testServer)
 async def self(interaction: discord.Interaction, name: str): 
     try:
         await interaction.response.send_message("The nation " + nations[name].name + " is owned by " + nations[name].owner + ".")
     except KeyError:
-        await interaction.response.send_message("That nation doesn't exist in the database.")
+        await interaction.response.send_message("That nation doesn't exist in the database.", ephemeral=True)
+
+
+# lists all the nations in the database
+@tree.command(name="nations", description="Lists all the nations in the database.", guild=testServer)
+async def self(interaction: discord.Interaction):
+    await interaction.response.send_message("The nations currently in the database are: " + ", ".join(stored.keys()) + ".")
+
+# deletes a nation from the database
+@tree.command(name="deletenation", description="Deletes a nation from the database.", guild=testServer)
+async def self(interaction: discord.Interaction, name: str):
+    try:
+        del nations[name]
+        del stored[name]
+        with open(nationFile, "wb") as tf:
+            pickle.dump(stored,tf)
+            tf.close()
+        await interaction.response.send_message("The nation " + name + " has been deleted from the database.")
+    except KeyError:
+        await interaction.response.send_message("That nation doesn't exist in the database.", ephemeral=True)
+
+# lists the allies of a nation in the database
+@tree.command(name="allies", description="Lists the allies of a nation in the database.", guild=testServer)
+async def self(interaction: discord.Interaction, name: str):
+    try:
+        await interaction.response.send_message(nations[name].getAllies())
+    except KeyError:
+        await interaction.response.send_message("That nation doesn't exist in the database.", ephemeral=True)
+
+
+# allies two nations both which are within the database
+@tree.command(name="ally", description="Allies two nations that are within the database.", guild=testServer)
+async def self(interaction: discord.Interaction, nation_name: str, allied_nation_name: str):
+        if len(nations[nation_name].allies) == 2:
+            await interaction.response.send_message(nations[nation_name].name + " cannot ally with any more nations.")
+            return
+        if allied_nation_name in nations:
+            if len(nations[allied_nation_name].allies) == 2:
+                await interaction.response.send_message(nations[allied_nation_name].name + " cannot ally with any more nations.")
+                return
+            nations[nation_name].ally(allied_nation_name)
+            nations[allied_nation_name].ally(nation_name)
+            stored[nations[nation_name].name]["allies"] = nations[nation_name].allies
+            stored[nations[allied_nation_name].name]["allies"] = nations[allied_nation_name].allies
+            with open(nationFile, "wb") as tf:
+                pickle.dump(stored,tf)
+                tf.close()
+            await interaction.response.send_message(nations[nation_name].name +  " is now allied with " + nations[allied_nation_name].name)
+        else:
+            await interaction.response.send_message("The nation " + allied_nation_name + " doesn't exist in the database.")
+
+# removes an alliance between two nations within the database
+@tree.command(name="removeally", description="Removes an alliance between two nations within the database.", guild=testServer)
+async def self(interaction: discord.Interaction, nation_name: str, allied_nation_name: str):
+    if allied_nation_name in nations:
+        if nations[allied_nation_name].name not in nations[nation_name].allies:
+            await interaction.response.send_message(nations[nation_name].name + " is not allied with " + nations[allied_nation_name].name)
+            return
+        nations[nation_name].removeAlly(allied_nation_name)
+        nations[allied_nation_name].removeAlly(nation_name)
+        stored[nations[nation_name].name]["allies"] = nations[nation_name].allies
+        stored[nations[allied_nation_name].name]["allies"] = nations[allied_nation_name].allies
+        with open(nationFile, "wb") as tf:
+            pickle.dump(stored,tf)
+            tf.close()
+        await interaction.response.send_message(nations[nation_name].name +  " is no longer allied with " + allied_nation_name)
+    else:
+        await interaction.response.send_message("The nation " + allied_nation_name + " doesn't exist in the database.")
+
+# changes a nation's owner
+@tree.command(name="setowner", description="Changes a nation's owner", guild=testServer)
+async def self(interaction:discord.Interaction, nation_name: str, new_owner: str):
+    nations[nation_name].setOwner(new_owner)
+    stored[nations[nation_name].name]["owner"] = nations[nation_name].owner
+    with open(nationFile, "wb") as tf:
+        pickle.dump(stored,tf)
+        tf.close()
+    await interaction.response.send_message(nations[nation_name].owner +  " is the new owner of " + nations[nation_name].name)
+
 
 
 
@@ -174,96 +254,19 @@ async def self(interaction: discord.Interaction, name: str):
  
 
 
+
+
+
+
+
+
     
 
-# all the information about a nation
-    if mg == "&nation": 
-        await message.channel.send(gnq1)
-        Nname = await client.wait_for("message", timeout=60.0)
-        try:
-            await message.channel.send("The nation " + nations[Nname.content].name + " is owned by " + nations[Nname.content].owner + ".")
-        except KeyError:
-            await message.channel.send("That nation doesn't exist in the database.")
 
-# lists all the nations in the database
-    if mg == "&nations":
-        await message.channel.send("The nations currently in the database are: " + ", ".join(stored.keys) + ".")
 
-# deletes a nation from the database
-    if mg == "&deleteNation":
-        await message.channel.send(gnq1)
-        Nname = await client.wait_for("message", timeout=60.0)
-        try:
-            del nations[Nname.content]
-            del stored[Nname.content]
-            with open(nationFile, "wb") as tf:
-                pickle.dump(stored,tf)
-                tf.close()
-            await message.channel.send("The nation " + Nname.content + " has been deleted from the database.")
-        except KeyError:
-            await message.channel.send("That nation doesn't exist in the database.")
 
-# lists the allies of a nation in the database
-    if mg == "&allies":
-        await message.channel.send(gnq1)
-        Nname = await client.wait_for("message", timeout=60.0)
-        await message.channel.send(nations[Nname.content].getAllies())
-    
-# allies two nations both which are within the database
-    if mg == "&ally":
-        await message.channel.send(gnq1)
-        Nname = await client.wait_for("message", timeout=60.0)
-        await message.channel.send("What nation are they allied with?")
-        Nally = await client.wait_for("message", timeout=60.0)
-        if len(nations[Nname.content].getAllies()) == 2:
-            await message.channel.send(nations[Nname.content].name + " cannot ally with any more nations.")
-            return
-        if Nally.content in nations:
-            if len(nations[Nally.content].getAllies()) == 2:
-                await message.channel.send(nations[Nally.content].name + " cannot ally with any more nations.")
-                return
-            if nations[Nally.content] not in nations[Nname.content].allies:
-                await message.channel.send(nations[Nname.content].name + " is not allied with " + nations[Nally.content].name)
-            nations[Nname.content].ally(Nally.content)
-            nations[Nally.content].ally(Nname.content)
-            stored[nations[Nname.content].name]["allies"] = nations[Nname.content].getAllies()
-            stored[nations[Nally.content].name]["allies"] = nations[Nally.content].getAllies()
-            with open(nationFile, "wb") as tf:
-                pickle.dump(stored,tf)
-                tf.close()
-            await message.channel.send(nations[Nname.content].name +  " is now allied with " + Nally.content)
-        else:
-            await message.channel.send("The nation " + Nally.content + " doesn't exist in the database.")
 
-# removes an alliance between two nations within the database
-    if mg == "&removeAlly":
-        await message.channel.send(gnq1)
-        Nname = await client.wait_for("message", timeout=60.0)
-        await message.channel.send("What nation were they allied with?")
-        Nally = await client.wait_for("message", timeout=60.0)
-        if Nally.content in nations:
-            nations[Nname.content].removeAlly(Nally.content)
-            nations[Nally.content].removeAlly(Nname.content)
-            stored[nations[Nname.content].name]["allies"] = nations[Nname.content].getAllies()
-            stored[nations[Nally.content].name]["allies"] = nations[Nally.content].getAllies()
-            with open(nationFile, "wb") as tf:
-                pickle.dump(stored,tf)
-                tf.close()
-            await message.channel.send(nations[Nname.content].name +  " is no longer allied with " + Nally.content)
-        else:
-            await message.channel.send("The nation " + Nally.content + " doesn't exist in the database.")
 
-# changes a nation's owner
-    if mg == "&setOwner":
-        await message.channel.send(gnq1)
-        Nname = await client.wait_for("message", timeout=60.0)
-        await message.channel.send("Who is the new owner?")
-        NNowner = await client.wait_for("message", timeout=60.0)
-        nations[Nname.content].setOwner(NNowner.content)
-        stored[nations[Nname.content].name]["owner"] = nations[Nname.content].owner
-        with open(nationFile, "wb") as tf:
-            pickle.dump(stored,tf)
-            tf.close()
 """
 try:
     client.run('id')
